2fc85fbb9e4dd69177b07f67c58b314299979fb9
Akshay Khot <akshay.khot@cambridgebrainsciences.com>
2022-11-18 12:22:08 -0800

    simplify

--- app.rb
+++ app.rb
@@ -5,18 +5,12 @@ class App
   def call(env)
     headers = { 'Content-Type' => 'text/html' }
     
-    title = get_title(env) # new code here
+    title = 'The To-Do List'
     template = ERB.new(template_html)
     tasks = Task.all
 
     response_html = template.result(binding)
     [200, headers, [response_html]]
-  end
-
-  def get_title(env)
-    query = env['QUERY_STRING'] # "title=ruby"
-    values = query.split('=')   # ["title", "ruby"]
-    values[1]                   # ruby
   end
 
   def template_html
